09df29b8cd44c012feed10248632a40b03949dfb
Damian <damianfullstack@gmail.com>
2026-07-29 01:57:15 -0400

    square regen logic, random color and dyn dimension

--- javascript.js
+++ javascript.js
@@ -1,4 +1,6 @@
 const body = document.querySelector(`body`);
+const regenBtn = document.querySelector(`button`);
+const dimensions = document.querySelector(`.dimensions`);
 const container = document.querySelector(`.container`);
 const CONTAINER_WIDTH = 550;
 const SQUARE_NUM_MAX = 100;
@@ -12,9 +14,28 @@ createGrid(squareNum);
 container.addEventListener(`mouseover`, paintPixel);
 function paintPixel(e){
     if(!e.target.classList.contains(`container`)){
-        e.target.style.backgroundColor = `red`;
+        e.target.style.backgroundColor = randomColor();
         console.log(`trigger`);
     }
+}
+
+regenBtn.addEventListener(`click`, regenerate);
+function regenerate(){
+    for(const child of container.children){
+        child.style.backgroundColor = `white`;
+    }
+    squareNum = -1;
+    while(true){
+        squareNum = Number(prompt(`Enter desired pixel width:`));
+        if(squareNum > 0 && squareNum <= 100 && Number.isInteger(squareNum)){
+            break;
+        }else{
+            alert(`Invalid input. Please choose an integer between 0 and 100 inclusive`);
+        }
+    }
+    dimensions.textContent = `${squareNum} x ${squareNum}`;
+    container.replaceChildren();
+    createGrid(squareNum);
 }
 
 function createGrid(squareNum){
@@ -26,3 +47,5 @@ function createGrid(squareNum){
         container.appendChild(square);
     }
 }
+
+function randomColor(){return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');}
